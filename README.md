ad1395abed093766dee2c39d7f8fa585915cf68c
gisosa <gio53196@gmail.com>
2021-07-06 16:20:16 -0700

    Added funtional Index and Score-related HTML files along with three JavaScript files that compose the main code, as well as stylized CSS sheet, sound effects, and README file

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Code-Quiz-Web-API-
-Built a timed coding quiz with multiple-choice questions by utilizing Web APIs
+Built a timed multiple-choice Coding-related quiz by utilizing Web APIs.
+
+## Description
+This assignment we utilized Web APIs in order to build a timed coding quiz with multiple-choice questions. The HTML and CSS are utilized with Javascript in order to have a clean and responsive user interface. 
+
+### Completed Tasks
+* HTML files made for Quiz template and Scroreboard
+* Javascript utilized to make Start Button function
+* Questions are created in JS file to be presented
+* Timer event added in script that deducts time/ score when answering questions
+* Scores JS file added to allow scores to be recorded and cleared
+* Comments made for organization and efficiency
+
+#### Link
+[Link to Deployed Website] (https://gisosa531.github.io/Code-Quiz-Web-API/Develop/index.html)
+
+[link to GitHub Repository] (https://github.com/gisosa531/Code-Quiz-Web-API)
+
+#### References
+*   [https://getbootstrap.com/docs/4.0/layout/grid/](https://getbootstrap.com/docs/4.0/layout/grid/)
+*   [https://www.w3schools.com/js/](https://www.w3schools.com/js/)
+*   [https://javascript.info/](https://javascript.info/)
+
+### Images
+
---
+++ assets/js/questions.js
@@ -0,0 +1,56 @@
+//  list of all questions, choices, and answers in array//
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes","parentheses"],
+        answer: "quotes"
+    },
+
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+
+    {
+        title: 'What is the correct syntax for referring to an external script called "LFC.js"?',
+        choices: ['<script src="LFC.js">', '<script source="LFC.js">', '<script ref="LFC.js">', '<script type="LFC.js">'],
+        answer: '<script src="LFC.js">'
+    },
+
+    {
+        title: "Which HTML attribute is used to define inline styles?",
+        choices: ["font","style", "class", "styles"],
+        answer: "style"
+    },
+
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<head>", "<meta>", "<style>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function:myFunction", "function myFunction", 'function""'],
+        answer: "function = myFunction()"
+    },
+];
